fe0873d7cc159bb8e85b244416f3da9ee38cdfdc
prafull <praguru14@gmail.com>
2022-03-21 07:26:10 +0530

    commit

--- src/main/java/com/smsc/sc/controller/BaseController.java
+++ src/main/java/com/smsc/sc/controller/BaseController.java
@@ -2,19 +2,62 @@ package com.smsc.sc.controller;
 
 
 import com.smsc.sc.HelloWorldBean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.smsc.sc.dao.UserDao;
+import com.smsc.sc.exception.UserException;
+import com.smsc.sc.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
 
 @RestController
 public class BaseController {
 
+    @Autowired
+    private UserDao service;
 
-    @GetMapping("/")
-    public String hello(){
-        return "hello world";
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
     }
-    @GetMapping("/hello")
-    public HelloWorldBean hello1(){
-        return new HelloWorldBean("Hello World");
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        User user = service.findOne(id);
+
+        if (user == null)
+            throw new UserException("id-" + id);
+
+        return user;
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        User user = service.deleteById(id);
+
+        if (user == null)
+            throw new UserException("id-" + id);
+    }
+
+    //
+    // input - details of user
+    // output - CREATED & Return the created URI
+    @PostMapping("/users")
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
+        User savedUser = service.save(user);
+        // CREATED
+        // /user/{id}     savedUser.getId()
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+
     }
 }
